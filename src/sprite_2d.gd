3a6f7b4bb2b1d5b9aceef35ca9e5744f5971abad
Dale Hagglund <dale.hagglund@gmail.com>
2026-06-26 01:44:29 -0600

    control head with keys, messing w/ gdscript-mode, weird uid issues
    Started messing with gdscript-mode in emacs
    
    - can launch main scene from inside emacs
    - colorized code and indenting, fairly cool.
    
    But, ran into some problems...
    
    - after launching main scene as above, when I went to a terminal and
      typed `godot`, I entered the scene directly, and couldn't get to the
      editor
    
    - still haven't figure out where that's stored, but `godot --editor`
      does the trick for now.
    
    - also got some complaints about missing uids, so probably I should
      have listend to everybody who told me that not having the svg and gd
      uid files around was a bad idea.
    
    - had to remove .godot/editor/filesystem_cache10 (kept a backup, with
      with a name like that it seemed likely to be safe), and that seemed to
      fix it up
    
    keyboard control...
    
    - oh, yeah, got control of the head: left / right arrows rotate it
      correspondingly, up moves it forward.
    
    - almost like a little spaceship...

--- src/sprite_2d.gd
+++ src/sprite_2d.gd
@@ -5,6 +5,23 @@ var angular_velocity = PI
 
 func _init():
 	print("Hello there, world!")
+	print("ZERO", Vector2.ZERO * 10)
+
+func pressed(key: String):
+	return Input.is_action_pressed(key)
+
+func rotation_direction():
+	if pressed("ui_left"):  return -1
+	if pressed("ui_right"): return +1
+	return 0
+
+func velocity_vector():
+	if not pressed("ui_up"): return Vector2.ZERO
+	return Vector2.UP 
 
 func _process(delta):
-	rotation += angular_velocity * delta
+	var dir = rotation_direction()
+	var vel = velocity_vector().rotated(rotation) * speed
+
+	rotation += dir * angular_velocity * delta
+	position += vel * delta
